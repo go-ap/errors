84de00d405cc22a03f25858e61939b83c7ec5b04
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-06-06 21:02:23 +0200

    Added test placeholders

---
+++ errors_test.go
@@ -0,0 +1,35 @@
+package errors
+
+import "testing"
+
+func TestAnnotatef(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNewf(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestErrorf(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestErr_As(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestErr_Error(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestErr_Location(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestErr_StackTrace(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestErr_Unwrap(t *testing.T) {
+	t.Skipf("TODO")
+}
---
+++ http_test.go
@@ -0,0 +1,243 @@
+package errors
+
+import "testing"
+
+func TestErrorHandlerFn_ServeHTTP(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestBadRequest_As(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestHandleError(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestBadRequest_Is(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestBadRequest_Unwrap(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestBadRequestf(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestChallenge(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestForbidden_As(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestForbidden_Is(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestForbidden_Unwrap(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestForbiddenf(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestHttpErrors(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestIsBadRequest(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestIsForbidden(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestIsMethodNotAllowed(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestIsNotFound(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestIsNotImplemented(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestIsNotSupported(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestIsNotValid(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestIsTimeout(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestIsUnauthorized(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestMethodNotAllowed_As(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestMethodNotAllowed_Is(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestMethodNotAllowed_Unwrap(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestMethodNotAllowedf(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNewBadRequest(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNewForbidden(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNewMethodNotAllowed(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNewNotFound(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNewNotImplemented(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNewNotSupported(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNewNotValid(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNewTimeout(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNewTimeoutf(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNewUnauthorized(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotFound_As(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotFound_Is(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotFound_Unwrap(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotFoundf(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotImplemented_As(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotImplemented_Is(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotImplemented_Unwrap(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotImplementedf(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotSupported_As(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotSupported_Is(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotSupported_Unwrap(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotSupportedf(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotValid_As(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotValid_Is(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotValid_Unwrap(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestNotValidf(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestRenderErrors(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestTimeout_As(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestTimeout_Is(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestTimeout_Unwrap(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestUnauthorized_As(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestUnauthorized_Challenge(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestUnauthorized_Is(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestUnauthorized_Unwrap(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestUnauthorizedf(t *testing.T) {
+	t.Skipf("TODO")
+}
